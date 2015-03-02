718ba52ea24d89c3ebce0b6df31317de7c601b73
Phibedy <philipp.schmette@gmx.de>
2015-03-02 22:43:39 +0100

    linux fix

--- lms/core/include/core/lms_exports.h
+++ lms/core/include/core/lms_exports.h
@@ -1,5 +1,9 @@
-#ifdef lms_EXPORTS
-#define lms_EXPORT __declspec(dllexport)
-#else
-#define lms_EXPORT __declspec(dllimport)
+#if(UNIX)
+    #ifdef lms_EXPORTS
+     #define lms_EXPORT __declspec(dllexport)
+    #else
+     #define lms_EXPORT __declspec(dllimport)
+    #endif
+#else(WIN32)
+    #define lms_EXPORT
 #endif
