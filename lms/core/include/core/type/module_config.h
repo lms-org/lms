5e68a54e07244cbeffeed33dd22b12e638422c09
Hans Kirchner <hans-kirchner@gmx.de>
2015-03-03 19:20:44 +0100

    New class: ModuleConfig (with loader)

---
+++ lms/core/include/core/type/module_config.h
@@ -0,0 +1,80 @@
+#ifndef LMS_TYPE_MODULE_CONFIG_H
+#define LMS_TYPE_MODULE_CONFIG_H
+
+#include <unordered_map>
+#include <sstream>
+
+namespace lms { namespace type {
+
+/**
+ * @brief ModuleConfig is a property map
+ * where modules can load their configuration from.
+ *
+ * @author Hans Kirchner
+ */
+class ModuleConfig {
+public:
+    /**
+     * @brief Load a config file from the given path.
+     *
+     * The format is KEY = VALUE.
+     * Empty lines are ignored.
+     * Comment lines start with '#'.
+     *
+     * @param path file path to the config file
+     * @return false if the file could not be opened, true otherwise
+     */
+    bool loadFromFile(const std::string &path);
+
+    /**
+     * @brief Return the value by the given config key.
+     *
+     * If the key is not found the default constructor
+     * of T is invoked the that object is returned.
+     *
+     * If you want to check if a key is in the config file
+     * use hasKey()
+     *
+     * @param key the key to look for
+     * @return value of type T
+     */
+    template<typename T>
+    T get(const std::string &key) const {
+        T result;
+        const auto it = properties.find(key);
+
+        if(it == properties.end()) {
+            return result;
+        } else {
+            std::istringstream stream(*it);
+            stream >> result;
+            return result;
+        }
+    }
+
+    /**
+     * @brief Check if the given key is available.
+     * @param key the key to look for
+     * @return true, if the key is found
+     */
+    bool hasKey(const std::string &key) const {
+        return properties.count(key) == 1;
+    }
+
+private:
+    std::unordered_map<std::string, std::string> properties;
+
+    /**
+     * Remove surrounding whitespace from a std::string.
+     * @param s The string to be modified.
+     * @param t The set of characters to delete from each end
+     * of the string.
+     * @return trimmed string
+     */
+    static std::string trim(const std::string& str, const std::string &delims = " \t\n\r\f\v");
+};
+
+} // namespace type
+} // namespace lms
+
+#endif /* LMS_TYPE_MODULE_CONFIG_H */
---
+++ lms/core/main/type/module_config.cpp
@@ -0,0 +1,87 @@
+#include <core/type/module_config.h>
+#include <fstream>
+#include <string>
+
+namespace lms { namespace type {
+
+/**
+ * @brief Specialization for string
+ * @param key the key to look for
+ * @return value of type string
+ */
+template<>
+std::string ModuleConfig::get<std::string>(const std::string &key) const {
+    const auto it = properties.find(key);
+
+    if(it == properties.end()) {
+        return "";
+    } else {
+        return it->second;
+    }
+}
+
+/**
+ * @brief Specialization for int
+ * @param key the key to look for
+ * @return value of type int
+ */
+template<>
+int ModuleConfig::get<int>(const std::string &key) const {
+    const auto it = properties.find(key);
+
+    if(it == properties.end()) {
+        return 0;
+    } else {
+        return std::atoi(it->second.c_str());
+    }
+}
+
+/**
+ * @brief Specialization for double
+ * @param key the key to look for
+ * @return value of type double
+ */
+template<>
+double ModuleConfig::get<double>(const std::string &key) const {
+    const auto it = properties.find(key);
+
+    if(it == properties.end()) {
+        return 0;
+    } else {
+        return std::atof(it->second.c_str());
+    }
+}
+
+bool ModuleConfig::loadFromFile(const std::string &path) {
+    std::ifstream in(path);
+
+    if(! in.is_open()) {
+        return false;
+    }
+
+    std::string line;
+
+    while(std::getline(in, line)) {
+        if(!line.empty() && line[0] != '#') {
+            size_t index = line.find_first_of('=');
+
+            if(index != std::string::npos) {
+                properties[trim(line.substr(0, index))] = trim(line.substr(index+1));
+            }
+        }
+    }
+
+    in.close();
+
+    return true;
+}
+
+std::string ModuleConfig::trim(const std::string& str, const std::string& delims) {
+    std::string result(str);
+    result.erase(0, result.find_first_not_of(delims));
+    result.erase(result.find_last_not_of(delims) + 1);
+    return result;
+}
+
+} // namespace type
+} // namespace lms
