7436fccdfb54501bdf2cc813e05fb5393a2b042c
Markus Herb <mail@markusherb.de>
2015-03-09 21:27:12 +0100

    Fix Framework::programDirectory on OS X

--- lms/core/main/framework.cpp
+++ lms/core/main/framework.cpp
@@ -13,6 +13,8 @@
 
 #ifdef _WIN32
     #include <Windows.h>
+#elif __APPLE__
+    #include <mach-o/dyld.h>
 #endif
 
 namespace lms{
@@ -130,23 +132,40 @@ std::string Framework::programDirectory(){
         //A std:string  using the char* constructor.
         std::string ss(ch);
         directory = ss;
+
+        
 #else
         char path[PATH_MAX];
         memset (path, 0, PATH_MAX);
+  #ifdef __APPLE__
+        uint32_t size = PATH_MAX;
+        if( _NSGetExecutablePath(path, &size) == 0 ) {
+            char* fullpath = realpath(path, NULL);
+            if( !fullpath ) {
+                perror("realpath failed");
+                exit(1);
+            }
+            directory = fullpath;
+        } else {
+            perror("_NSGetExecutablePath failed");
+            exit(1);
+        }
+  #else
         if (readlink("/proc/self/exe", path, PATH_MAX) == -1) {
             perror("readlink failed");
             exit(1);
         }
+        directory = path;
+  #endif
         //get programmdirectory
         // TODO optimize this a bit
-        directory = path;
         directory = directory.substr(0, directory.rfind("/"));
         directory = directory.substr(0, directory.rfind("/"));
         directory = directory + "/";
 #endif
     }
     
-    //std::cout << "ProgramDirectory: " << directory << std::endl;
+    // std::cout << "ProgramDirectory: " << directory << std::endl;
     return directory;
 }
 
