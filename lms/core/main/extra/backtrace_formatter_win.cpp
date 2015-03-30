dee4c282c39ff302ab9a862bbccb7b60ff979a12
Hans Kirchner <hans-kirchner@gmx.de>
2015-03-30 12:37:58 +0200

    Implement BacktraceFormatter for Win32 (NOT tested)

--- lms/core/main/extra/backtrace_formatter_win.cpp
+++ lms/core/main/extra/backtrace_formatter_win.cpp
@@ -1,11 +1,38 @@
-#include "backtrace_formatter.h"
+#include "lms/extra/backtrace_formatter.h"
 #include <iostream>
+#include <Windows.h>
 
 namespace lms {
 namespace extra {
 
-void BacktraceFormatter::print(){
-    std::cerr << "BacktraceFormatter::print not implemented on Win32" << std::endl;
+void printStacktrace() {
+    // http://stackoverflow.com/questions/5693192/win32-backtrace-from-c-code
+    // https://msdn.microsoft.com/en-us/library/windows/desktop/bb204633(v=vs.85).aspx
+
+    unsigned int   i;
+    void         * stack[ 100 ];
+    unsigned short frames;
+    SYMBOL_INFO  * symbol;
+    HANDLE         process;
+
+    process = GetCurrentProcess();
+
+    SymInitialize( process, NULL, TRUE );
+
+    frames               = CaptureStackBackTrace( 0, 100, stack, NULL );
+    symbol               = ( SYMBOL_INFO * )calloc( sizeof( SYMBOL_INFO ) + 256 * sizeof( char ), 1 );
+    symbol->MaxNameLen   = 255;
+    symbol->SizeOfStruct = sizeof( SYMBOL_INFO );
+
+    printf("\nStacktrace\n");
+
+    for( i = 0; i < frames; i++ ) {
+        SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
+
+        printf( "%i: %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address );
+    }
+
+    free( symbol );
 }
 
 }  // namespace extra
