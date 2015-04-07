8107b5a8c005aaefa085d8440f75aa57527bcd3e
Hans Kirchner <hans-kirchner@gmx.de>
2015-04-07 14:52:27 +0200

    Add comments for Clock

--- lms/core/include/lms/clock.h
+++ lms/core/include/lms/clock.h
@@ -1,5 +1,5 @@
-#ifndef LMS_SCHEDULER_H
-#define LMS_SCHEDULER_H
+#ifndef LMS_CLOCK_H
+#define LMS_CLOCK_H
 
 #include <array>
 
@@ -8,10 +8,36 @@
 
 namespace lms {
 
+/**
+ * @brief Used in the main loop of class Framework this
+ * class measures the time of each iterations and
+ * ensures a given clock rate by sleeping.
+ *
+ * Clock will measure:
+ * - loop condition
+ * - loop body, including the calls to beforeLoopIteration and afterLoopIteration
+ * - the call to sleep (usually included in afterLoopIteration)
+ */
 class Clock {
 public:
+    /**
+     * @brief Create a new clock given the given clock time
+     * @param rootLogger the clock will issue warnings if main
+     * loop is to slow
+     * @param loopTime duration of a single loop iteration
+     */
     Clock(logging::Logger &rootLogger, extra::PrecisionTime loopTime);
+
+    /**
+     * @brief Should be called directly at the beginning
+     * of the main loop's body.
+     */
     void beforeLoopIteration();
+
+    /**
+     * @brief Should be called at the end of the
+     * main loop's body.
+     */
     void afterLoopIteration();
 private:
     logging::ChildLogger logger;
@@ -27,4 +53,4 @@ private:
 
 }  // namespace lms
 
-#endif /* LMS_SCHEDULER_H */
+#endif /* LMS_CLOCK_H */
