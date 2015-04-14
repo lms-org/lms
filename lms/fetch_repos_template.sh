bbc392cc529a0f325fb18c4f7f38d604fc0e99e7
Hans Kirchner <hans-kirchner@gmx.de>
2015-04-14 11:46:15 +0200

    New script: fetch_repos.sh, Fix whitespace

--- lms/fetch_repos_template.sh
+++ lms/fetch_repos_template.sh
@@ -2,15 +2,15 @@
 
 SOURCE_DIR="@CMAKE_SOURCE_DIR@"
 
-pullAllRepos() {
+fetchAllRepos() {
     for MOD in $(find "$SOURCE_DIR/$1" -maxdepth 1 -mindepth 1 -type d -printf "%P\n")
     do
         cd "$SOURCE_DIR/$1/$MOD"
         echo $MOD
-        git pull
+        git fetch
     done
 }
 
-pullAllRepos "external/modules"
-pullAllRepos "external/libraries"
+fetchAllRepos "external/modules"
+fetchAllRepos "external/libraries"
 
