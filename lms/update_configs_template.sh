773340c126c1032df36c2def9d526575dd077923
Hans Kirchner <hans-kirchner@gmx.de>
2015-04-22 12:55:37 +0200

    Fix update_configs.sh to not copy anything in bin dir

--- lms/update_configs_template.sh
+++ lms/update_configs_template.sh
@@ -3,22 +3,23 @@
 SOURCE_DIR="@CMAKE_SOURCE_DIR@"
 BINARY_DIR="@CMAKE_BINARY_DIR@"
 
+# $1 - file to copy from source to binary directory
 copyFile() {
     mkdir -p -v $(dirname "$BINARY_DIR/$1")
     cp -u -v "$SOURCE_DIR/$1" "$BINARY_DIR/$1"
 }
 
-for FILE in $(find "$SOURCE_DIR/configs" -name "*.xml" -printf "%P\n")
+for FILE in $(find "$SOURCE_DIR/configs" -name "*.xml" -not -path "$BINARY_DIR/*" -printf "%P\n")
 do
     copyFile "configs/$FILE"
 done
 
-for FILE in $(find $SOURCE_DIR -name "*.lconf" -printf "%P\n")
+for FILE in $(find $SOURCE_DIR -name "*.lconf" -not -path "$BINARY_DIR/*" -printf "%P\n")
 do
     copyFile $FILE
 done
 
-for FILE in $(find $SOURCE_DIR -name "loadConfig.xml" -printf "%P\n")
+for FILE in $(find $SOURCE_DIR -name "loadConfig.xml" -not -path "$BINARY_DIR/*" -printf "%P\n")
 do
     copyFile $FILE
 done
