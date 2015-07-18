ec8662ad7a39fdb14d592af096b49c749b41f165
Hans Kirchner <hans-kirchner@gmx.de>
2015-07-18 17:45:18 +0200

    Improve framework.xsd and validation script

--- validate-xml.sh
+++ validate-xml.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
 # For xsltproc install package xsltproc
-# For xmllint install package libxml-utils
+# For xmllint install package libxml2-utils
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   echo "Usage: validate-xml.sh <file.xml>"
   exit 1
 fi
 
-xsltproc preprocess.xsl "$1" | xmllint --noout --schema framework.xsd -
+validate() {
+  xsltproc preprocess.xsl "$1" | xmllint --noout --schema framework.xsd -
+}
+
+# green
+highlightColor="\033[0;32m"
+highlightReset="\033[0m"
+
+if [ -d "$1" ]; then
+  for file in $(find "$1" -type f -iname '*.xml' -printf '%P\n'); do
+    echo "${highlightColor}${file}${highlightReset}"
+    validate "$1/$file"
+  done
+else
+  validate "$1"
+fi
