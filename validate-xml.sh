e92afcffc7d2a3a9c46888f5c4cfbda61aad7f5d
Hans Kirchner <hans-kirchner@gmx.de>
2015-08-29 16:57:41 +0200

    Improve validate-xml.sh

--- validate-xml.sh
+++ validate-xml.sh
@@ -8,17 +8,24 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+# check if stdout is a terminal...
+if [ -t 1 ]; then
+  # see if it supports colors...
+  ncolors=$(tput colors)
+
+  if test -n "$ncolors" && test ${ncolors} -ge 8; then
+    normal="$(tput sgr0)"
+    green="$(tput setaf 2)"
+  fi
+fi
+
 validate() {
   xsltproc preprocess.xsl "$1" | xmllint --noout --schema framework.xsd -
 }
 
-# green
-highlightColor="\033[0;32m"
-highlightReset="\033[0m"
-
 if [ -d "$1" ]; then
   for file in $(find "$1" -type f -iname '*.xml' -printf '%P\n'); do
-    echo "${highlightColor}${file}${highlightReset}"
+    echo "${green}${file}${normal}"
     validate "$1/$file"
   done
 else
