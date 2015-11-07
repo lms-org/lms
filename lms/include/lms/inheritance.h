765a3245124ce6322ecf060e0ff78abaf64ffa88
phibedy <philipp.schmette@gmx.de>
2015-11-07 12:48:14 +0100

    working inheritance <3

--- lms/include/lms/inheritance.h
+++ lms/include/lms/inheritance.h
@@ -41,7 +41,7 @@ struct Impl<First, Args...>
             return true;
         //go deeper in the tree
         if(std::is_base_of<InheritanceBase,First>::value){
-            InheritanceBase *inh = static_cast<InheritanceBase*>(new First());
+            InheritanceBase *inh = (InheritanceBase*)(new First());
             sub = inh->isSubType(hashcode);
             if(sub)
                 return true;
@@ -51,7 +51,13 @@ struct Impl<First, Args...>
   }
 };
 
-
+/**
+ * @brief The Inheritance class
+ * extend this class to provide dataChannel type-casting and [insert word]*
+ * As I don't know the word for here is a small Example
+ * A: B{}
+ * -> one module accesses A, one accesses B, the dataChannel of type B will be created, module 1 will get dataChannel with type A but the object will be type B.
+ */
 class Inheritance:public InheritanceBase{
 public:
     /**
@@ -63,21 +69,6 @@ public:
     bool isSubType(size_t hashcode){
         return Impl<REST...>::isSubType(hashcode);
     }
-};
-
-//TEST
-
-class A{
-
-};
-
-class B:lms::Inheritance{
-public:
-
-    bool isSubType(size_t hashcode) override{
-        return Inheritance::isSubType<A>(hashcode);
-    }
-
 };
 
 
