1656b29a4e41ef7f43a42bb7e4bb498acae4a90a
Hans Kirchner <hans-kirchner@gmx.de>
2016-03-12 03:31:49 +0100

    Rename DEPRECATED to LMS_DEPRECATED

--- lms/include/lms/deprecated.h
+++ lms/include/lms/deprecated.h
@@ -1,13 +1,12 @@
 /**
   *Taken from http://stackoverflow.com/questions/295120/c-mark-as-deprecated/21265197#21265197
   */
-#ifndef DEPRECATED
+#ifndef LMS_DEPRECATED
     #ifdef __GNUC__
-        #define DEPRECATED __attribute__((deprecated))
+        #define LMS_DEPRECATED __attribute__((deprecated))
     #elif defined(_MSC_VER)
-        #define DEPRECATED __declspec(deprecated)
+        #define LMS_DEPRECATED __declspec(deprecated)
     #else
-        #pragma message("WARNING: You need to implement DEPRECATED for this compiler")
-        #define DEPRECATED
+        #define LMS_DEPRECATED
     #endif
 #endif
