d800397b395b8d256b014c962a446672382dec5b
phibedy <philipp.schmette@gmx.de>
2016-10-19 18:26:09 +0200

    conan test 2

--- conanfile.py
+++ conanfile.py
@@ -6,9 +6,11 @@ class LMSConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     exports = "include/*","src/*","README.md","CMakeLists.txt","cmake/*","CMakeData.txt"
     requires = "gtest/1.8.0@lms/stable","pugixml/1.7@lms/stable","tclap/1.2.1@lms/stable","Protobuf/2.6.1@memsharded/testing"
-    generators = "cmake"
+    generators = "cmake","txt"
 
     def build(self):
+        library_path = ":".join(self.deps_cpp_info.lib_paths)
+        self.run('LD_LIBRARY_PATH=%s ./bin/protoc ../messages/messages.proto --proto_path=../messages --cpp_out="../include"' % library_path)
         self.run('./bin/protoc ../messages/messages.proto --proto_path=../messages --cpp_out="../include"')
         cmake = CMake(self.settings)
         self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
